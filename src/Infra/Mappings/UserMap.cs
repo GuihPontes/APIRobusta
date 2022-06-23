1d6209f6615a799e49901daead4d6334e588ec6b
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-23 13:42:10 -0300

    Mapeando User para o banco de dados

--- src/Infra/Mappings/UserMap.cs
+++ src/Infra/Mappings/UserMap.cs
@@ -13,7 +13,32 @@ namespace Infra.Mappings
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            
+            builder.HasKey(x => x.Id);
+
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn()
+                .HasColumnType("BIGINT");
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(80)
+                .HasColumnName("Nome")
+                .HasColumnType("VARCHAR(80)");
+
+            builder.Property(x => x.Password)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnName("password")
+                .HasColumnType("VARCHAR(30)");
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(180)
+                .HasColumnName("email")
+                .HasColumnType("VARCHAR(180)");
+
+
         }
     }
 }
