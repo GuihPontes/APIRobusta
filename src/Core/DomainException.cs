dbc9f2a95c994b74b8eb70b4d83c7238fb84e2aa
Guilherme <guilherme-pontes2015@hotmail.com>
2022-06-23 14:14:01 -0300

    Tratamento de erro

---
+++ src/Core/DomainException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class DomainException : Exception
+
+    {
+        internal List<string> _erros;
+        public List<string> Erros => _erros;
+
+        public DomainException() { }
+
+        public DomainException(string mensage, List<string>erros) : base(mensage)
+        {
+            _erros = erros;
+        }
+
+        public DomainException(string message) : base(message) 
+        { }
+
+        public DomainException(string message,Exception innerException) : base(message, innerException)
+        { }
+
+    }
+}
